ce008ba4181cfdd712bb6e42e5f4807c51823952
Ибрагимжонов Исломбек Илхомжон угли <unkiborg@gmail.com>
2021-12-28 11:38:34 +0500

    Деструктуризация объектов

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-function getInfo() {
-  return ["BMW", "X3"];
-}
+// Деструктуризация функции
+// function getInfo() {
+//   return ["BMW", "X3"];
+// }
 
-const [carName, CarSeries] = getInfo();
+// const [carName, сarSeries] = getInfo();
 
-console.log(getInfo);
+// console.log(getInfo);
+
+// Деструктуризация объекта
+const link = {
+  href: "#",
+  title: "simple link",
+  target: "blank",
+  className: "link",
+  id: null,
+  children: {
+    span: {
+      content: "Click me",
+      className: "anchor",
+    },
+  },
+};
+
+const {
+  target,
+  className: renamed,
+  children: { span: { content, className } } = {},
+} = link;
