18e1df5e074d7fa4a36e54f63170cf8fa486760d
Ибрагимжонов Исломбек Илхомжон угли <unkiborg@gmail.com>
2021-12-28 13:02:14 +0500

    Формат JSON

--- main.js
+++ main.js
@@ -8,22 +8,54 @@
 // console.log(getInfo);
 
 // Деструктуризация объекта
-const link = {
-  href: "#",
-  title: "simple link",
-  target: "blank",
-  className: "link",
-  id: null,
-  children: {
-    span: {
-      content: "Click me",
-      className: "anchor",
-    },
-  },
-};
+// const link = {
+//   href: "#",
+//   title: "simple link",
+//   target: "blank",
+//   className: "link",
+//   id: null,
+//   children: {
+//     span: {
+//       content: "Click me",
+//       className: "anchor",
+//     },
+//   },
+// };
 
-const {
-  target,
-  className: renamed,
-  children: { span: { content, className } } = {},
-} = link;
+// const {
+//   target,
+//   className: renamed,
+//   children: { span: { content, className } } = {},
+// } = link;
+
+// Формат JSON
+const players = [
+  {
+    id: 1,
+    name: "Andres",
+    surname: "Iniesta",
+    club: "Vissel Cobe",
+  },
+  {
+    id: 2,
+    name: "Eden",
+    surname: "Hazard",
+    club: "Real Madrid",
+  },
+  {
+    id: 3,
+    name: "Mo",
+    surname: "Salah",
+    club: "Liverpool",
+  },
+  {
+    id: 4,
+    name: "Lionel",
+    surname: "Messi",
+    club: "Barcelona",
+  },
+];
+
+const jsonPlayers = JSON.stringify(players);
+
+console.log(jsonPlayers);
